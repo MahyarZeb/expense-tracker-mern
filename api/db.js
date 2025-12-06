b50ae7e63664065f380980bf4df865fecc2437f3
abcd <abcd@gmail.com>
2025-12-06 22:58:54 +0500

    Prepare for Vercel deployment

--- api/db.js
+++ api/db.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
-let isConnected = false;
+let isConnected;
 
 const connectDB = async () => {
   if (isConnected) return;
-
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    isConnected = conn.connections[0].readyState;
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    isConnected = db.connections[0].readyState;
     console.log("MongoDB Connected");
   } catch (err) {
-    console.error("MongoDB connection error:", err.message);
+    console.error("MongoDB connection error:", err);
+    throw err;
   }
 };
 
