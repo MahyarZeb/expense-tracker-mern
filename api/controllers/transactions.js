94480e72199d0212a46bbf4696a3ee07c8b956d6
abcd <abcd@gmail.com>
2025-12-06 21:31:26 +0500

    Resolve issues

--- api/controllers/transactions.js
+++ api/controllers/transactions.js
@@ -1,4 +1,4 @@
-const Transaction = require('../models/transactions');
+const Transaction = require('../models/Transaction');
 
 // @desc    Get all transactions
 // @route   GET /api/v1/transactions
