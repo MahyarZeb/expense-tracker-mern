04febf5dacc64385030e7e6e02b60af7b9307b18
abcd <abcd@gmail.com>
2025-12-06 23:10:03 +0500

    Resolve issues

--- api/db.js
+++ api/db.js
@@ -1,20 +1,24 @@
 const mongoose = require("mongoose");
 
-let isConnected;
+let cached = global.mongoose;
 
-const connectDB = async () => {
-  if (isConnected) return;
-  try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    isConnected = db.connections[0].readyState;
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-    throw err;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      .then((mongoose) => mongoose);
   }
-};
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 module.exports = connectDB;
